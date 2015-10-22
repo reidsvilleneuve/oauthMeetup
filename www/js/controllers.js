ea211eff47765a401b4ba1f9c26709a5c3d9e817
Reid Villeneuve <reidsvilleneuve@gmail.com>
2015-10-21 20:44:31 -0600

    Completed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,32 @@
 angular.module('starter')
-  .controller('login', function() {
+  .controller('login', function($http, $cordovaOauth) {
     var vm = this;
     console.log('Hello!');
+
+    vm.facebookLogin = facebookLogin;
+
+    function facebookLogin() {
+      $cordovaOauth.facebook('753481421462159', ['public_profile', 'email', 'user_photos'])
+        .then(function(result) {
+          console.log('cOAuth Success: ' + JSON.stringify(result));
+          getProfile(result.access_token)
+        })
+        .catch(function(error) {
+          console.log('cOAuth Error: ' + JSON.stringify(error));
+        });
+    }
+
+    function getProfile(token) {
+      var params = {
+        access_token: token,
+        fields: "id,name,gender,picture.height(961)",
+        format: "json"
+      };
+
+      $http.get("https://graph.facebook.com/v2.2/me", {params: params})
+        .then(function(result) {
+          vm.profile = result.data;
+          vm.profile.picture = vm.profile.picture.data.url;
+        });
+    }
   });
